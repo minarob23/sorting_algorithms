be77f5eaffa3a0ba441e592d1ddc38b9e4c1bd22
Mina Robir <menarobir246rm@gmail.com>
2023-09-25 17:42:44 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,6 +9,7 @@
 void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
+    int temp;
     int swapped;
 
     if (array == NULL || size <= 1)
@@ -22,15 +23,16 @@ void bubble_sort(int *array, size_t size)
         {
             if (array[j] > array[j + 1])
             {
-                int temp = array[j];
+                temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
                 swapped = 1;
-                print_array(array, size);  
             }
         }
 
         if (swapped == 0)
             break;
+
+        print_array(array, size);  
     }
 }
