a586cc3f52ccd3e9a396e5a3398a6f1604325200
Mina Robir <menarobir246rm@gmail.com>
2023-09-25 19:11:18 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -22,4 +22,8 @@ void print_list(const listint_t *list);
 listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void print_array(int *array, size_t size)
 #endif
