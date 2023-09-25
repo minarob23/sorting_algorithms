6d1c2d7c35c4d1fdb4af93248c3bac9a0a9dd6a8
minarob23 <menarobir246rm@gmail.com>
2023-09-25 09:07:58 -0700

    mina

---
+++ 3-quick_sort.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick sort
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size <= 1)
+        return;
+
+    quick_recursive(array, 0, size - 1, size);
+}
+
+/**
+ * quick_recursive - Recursively sorts an array using Quick sort
+ *
+ * @array: The array to be sorted
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: Number of elements in @array
+ */
+void quick_recursive(int *array, int low, int high, size_t size)
+{
+    if (low < high)
+    {
+        int pivot = lomuto_partition(array, low, high, size);
+        quick_recursive(array, low, pivot - 1, size);
+        quick_recursive(array, pivot + 1, high, size);
+    }
+}
+
+/**
+ * lomuto_partition - Partitions an array using the Lomuto partition scheme
+ *
+ * @array: The array to be partitioned
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: Number of elements in @array
+ * Return: The index of the pivot element
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = low - 1;
+    int j, temp;
+
+    for (j = low; j <= high - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+
+            // Print the array after each swap
+            print_array(array, size);
+        }
+    }
+
+    temp = array[i + 1];
+    array[i + 1] = array[high];
+    array[high] = temp;
+
+    // Print the array after the final swap
+    print_array(array, size);
+
+    return (i + 1);
+}
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(int *array, size_t size)
+{
+    size_t i;
+
+    for (i = 0; i < size; i++)
+    {
+        printf("%d", array[i]);
+        if (i < size - 1)
+            printf(", ");
+    }
+    printf("\n");
+}
+
