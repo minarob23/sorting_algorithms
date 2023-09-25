10d81fbfbdfc5fe1d05c023eedda89e2dd1c95d6
Mina Robir <menarobir246rm@gmail.com>
2023-09-25 18:18:52 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -20,5 +20,5 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
-void insert_sorted(listint_t **list, listint_t *node)
+listint_t *swap_node(listint_t *node, listint_t **list)
 #endif
