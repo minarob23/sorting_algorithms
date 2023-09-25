140ad830a6ffa1ffb882ba275bbce13ba6b0304d
Mina Robir <menarobir246rm@gmail.com>
2023-09-25 18:19:56 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,73 +1,46 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                      in ascending order using Insertion sort
- *
- * @list: Pointer to the head of the list
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: Dobule linked list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	listint_t *node;
 
-    listint_t *current, *prev, *temp;
-
-    current = (*list)->next;
-
-    while (current)
-    {
-        prev = current->prev;
-
-        while (prev && prev->n > current->n)
-        {
-            temp = prev->prev;
-
-            if (prev->next)
-                prev->next->prev = prev;
-
-            prev->next = current->next;
-            current->prev = prev->prev;
-            current->next = prev;
-            prev->prev = current;
-
-            if (current->prev)
-                current->prev->next = current;
-            else
-                *list = current;
-
-            print_list(*list);  // Print the list after each swap
-
-            prev = temp;
-        }
-
-        current = current->next;
-    }
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
 }
-
 /**
- * swap_node - Swaps a node with its previous one in a linked list
- *
- * @node: Node to be swapped
- * @list: Pointer to the head of the list
- * Return: Pointer to the node that was swapped
+ *swap_node - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
  */
 listint_t *swap_node(listint_t *node, listint_t **list)
 {
-    listint_t *back = node->prev;
-    listint_t *current = node;
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
 
-    back->next = current->next;
-    if (current->next)
-        current->next->prev = back;
-    current->next = back;
-    current->prev = back->prev;
-    back->prev = current;
-
-    if (current->prev)
-        current->prev->next = current;
-    else
-        *list = current;
-
-    return (current);
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
 }
