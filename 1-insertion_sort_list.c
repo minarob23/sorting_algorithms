49b8fee67af7af835ed635463e4ce540337761f6
Mina Robir <menarobir246rm@gmail.com>
2023-09-25 18:17:18 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,60 +1,46 @@
-#include <stdio.h>
 #include "sort.h"
-
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                      in ascending order using Insertion sort
- *
- * @list: Pointer to the head of the list
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: Dobule linked list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	listint_t *node;
 
-    listint_t *sorted, *current;
-
-    sorted = (*list)->next;
-
-    while (sorted)
-    {
-        current = sorted;
-        sorted = sorted->next;
-
-        insert_sorted(list, current);  // Insert the current node into the sorted part
-        print_list(*list);            // Print the list after each insertion
-    }
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
 }
-
 /**
- * insert_sorted - Inserts a node into the sorted part of a linked list
- *
- * @list: Pointer to the head of the list
- * @node: Node to be inserted
+ *swap_node - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
  */
-void insert_sorted(listint_t **list, listint_t *node)
+listint_t *swap_node(listint_t *node, listint_t **list)
 {
-    listint_t *current;
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
 
-    if (*list == NULL || node->n <= (*list)->n)
-    {
-        node->next = *list;
-        node->prev = NULL;
-        if (*list)
-            (*list)->prev = node;
-        *list = node;
-    }
-    else
-    {
-        current = *list;
-
-        while (current->next && current->next->n < node->n)
-            current = current->next;
-
-        node->next = current->next;
-        node->prev = current;
-        if (current->next)
-            current->next->prev = node;
-        current->next = node;
-    }
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
 }
